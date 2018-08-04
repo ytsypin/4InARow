8090fb05940bebc505d8ae1f7942d506c6d58db7
ytsypin <ytsypin@gmail.com>
2018-08-04 22:14:30 +0300

    XML, Exceptions

--- gameLogic/src/Exceptions/InvalidNumberOfColsException.java
+++ gameLogic/src/Exceptions/InvalidNumberOfColsException.java
@@ -3,6 +3,8 @@ package Exceptions;
 import java.math.BigInteger;
 
 public class InvalidNumberOfColsException extends Throwable {
+    int colValue;
     public InvalidNumberOfColsException(int cols) {
+        colValue = cols;
     }
 }
--- gameLogic/src/Exceptions/InvalidTargetException.java
+++ gameLogic/src/Exceptions/InvalidTargetException.java
@@ -1,6 +1,8 @@
 package Exceptions;
 
 public class InvalidTargetException extends Throwable {
+    int nValue;
     public InvalidTargetException(int n) {
+        nValue = n;
     }
 }
